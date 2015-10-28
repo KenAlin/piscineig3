60e47812a61660b00c43166fdaa88053ab656f4c
KenAlin <kek.serv@gmail.com>
2015-10-28 22:02:02 +0100

    Ajout texte
    (indicatif, hein)

--- content/template/core_header.php
+++ content/template/core_header.php
@@ -1,10 +1,10 @@
 <header>
   <div id="header_bandeau">
-
+    Remplacer ce texte par une image (*plz*)
   </div>
 
   <div id="header_menu">
-
+    Y ajouter des petits boutons, en fonction de si on est connecté ou pas (et longue vie à vos yeux)
   </div>
 
 </header>
