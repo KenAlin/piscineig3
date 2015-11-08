39522c7729618a64e4e68c16c620099d69a70aa0
KenAlin <kek.serv@gmail.com>
2015-11-08 19:55:13 +0100

    Meilleur alignement champs de connexion

--- content/modules/login.php
+++ content/modules/login.php
@@ -1,7 +1,15 @@
 <form class="loginForm" action="login" method="post">
+  <div class="elementFormLogin">
+    <p>Saisissez vos identifiants de membre pour vous connecter et accéder aux service de la ludothèque.</p>
+  </div>
   <input type="hidden" name="action" value="connexion">
-  <label for="pseudo" class="labelConnexion">Pseudo</label><input type="text" name="pseudo"><br>
-  <label for="pass" class="labelConnexion">Mot de passe</label><input type="password" name="pass">
+  <div class="elementFormLogin">
+    <label for="pseudo" class="labelConnexion">Pseudo</label><input type="text" name="pseudo">
+  </div>
+  <br>
+  <div class="elementFormLogin">
+    <label for="pass" class="labelConnexion">Mot de passe</label><input type="password" name="pass">
+  </div>
   <br>
 
   <input type="submit" value="Se connecter">
